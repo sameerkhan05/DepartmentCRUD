942353de42bdbdefdd00b43322864e3d94d3da40
SameerKhan <sameerkhanyt09@gmail.com>
2024-08-31 00:16:22 +0530

    started

---
+++ src/main/java/com/departmentProject/entities/DepartmentEntity.java
@@ -0,0 +1,30 @@
+package com.departmentProject.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Department")
+public class DepartmentEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private LocalDateTime createdAt;
+	@JsonProperty("isActive")
+	private Boolean isActive;
+	private String password;
+	private Integer primeNumber;
+
+}
---
+++ src/test/java/com/departmentProject/DepartmentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departmentProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
