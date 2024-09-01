a76f77433b6a3efad2ccd7c0ea65ce99ab87225a
SameerKhan <sameerkhanyt09@gmail.com>
2024-09-01 16:31:33 +0530

    advice completed

--- src/main/java/com/departmentProject/advices/ApiResponse.java
+++ src/main/java/com/departmentProject/advices/ApiResponse.java
@@ -1,5 +1,32 @@
 package com.departmentProject.advices;
 
-public class ApiResponse {
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+
+	@JsonFormat(pattern = "hh:mm:ss DD-MM-YYYY")
+	private LocalDateTime timeStamp;
+	private T data;
+	private ApiError error;
+
+	public ApiResponse() {
+		this.timeStamp = LocalDateTime.now();
+	}
+
+	public ApiResponse(T data) {
+		this();
+		this.data = data;
+	}
+
+	public ApiResponse(ApiError error) {
+		this();
+		this.error = error;
+	}
 
 }
---
+++ src/main/java/com/departmentProject/advices/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.departmentProject.advices;
+
+import com.departmentProject.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse<?>> handleDepartmentNotFound(ResourceNotFoundException exception){
+		ApiError apiError = ApiError.builder()
+				.status(HttpStatus.NOT_FOUND)
+				.message(exception.getMessage())
+				.build();
+		return buildErrorResponseEntity(apiError);
+	}
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse<?>> hadelInputValidation(MethodArgumentNotValidException exception){
+		List<String> errors = exception
+				.getBindingResult()
+				.getAllErrors()
+				.stream()
+				.map(error -> error.getDefaultMessage())
+				.collect(Collectors.toList());
+		ApiError apiError = ApiError.builder()
+				.status(HttpStatus.BAD_REQUEST)
+				.message("Input validation error")
+				.subError(errors)
+				.build();
+		return buildErrorResponseEntity(apiError);
+	}
+	private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError){
+		return new ResponseEntity<>(new ApiResponse<>(apiError),apiError.getStatus());
+	}
+}
