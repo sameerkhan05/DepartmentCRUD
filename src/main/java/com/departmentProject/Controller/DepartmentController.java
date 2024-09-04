a4abe016d06f6b9ed1b15ce42d3beb5c206ce6eb
SameerKhan <sameerkhanyt09@gmail.com>
2024-09-04 18:43:41 +0530

    completed

--- src/main/java/com/departmentProject/Controller/DepartmentController.java
+++ src/main/java/com/departmentProject/Controller/DepartmentController.java
@@ -4,10 +4,12 @@ import com.departmentProject.dto.DepartmentDTO;
 import com.departmentProject.exceptions.ResourceNotFoundException;
 import com.departmentProject.services.DepartmentService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -20,15 +22,42 @@ public class DepartmentController {
 		this.departmentService = departmentService;
 	}
 
-	@GetMapping("/{departmentID")
-	public ResponseEntity<DepartmentDTO> getDepartmentByID(@PathVariable(name = "departmentID") @Valid long id) {
+	@GetMapping("/{departmentId}")
+	public ResponseEntity<DepartmentDTO> getDepartmentByID(@PathVariable(name = "departmentId") @Valid long id) {
 		Optional<DepartmentDTO> departmentDTO = departmentService.getDepartmentByID(id);
 		return departmentDTO
 				.map(departmentDTO1 -> ResponseEntity.ok(departmentDTO1))
 				.orElseThrow(() -> new ResourceNotFoundException("Department not found by iD : " + id));
 	}
+
 	@GetMapping
-	public ResponseEntity<List<DepartmentDTO>> getAllDepartments(@RequestParam(required = false)@Valid String title){
+	public ResponseEntity<List<DepartmentDTO>> getAllDepartments(@RequestParam(required = false) @Valid String title) {
 		return ResponseEntity.ok(departmentService.getAllDepartments());
 	}
+
+	@PostMapping
+	public ResponseEntity<DepartmentDTO> createNewDepartment(@RequestBody @Valid DepartmentDTO inputDepartment) {
+		DepartmentDTO savedDepartment = departmentService.createNewDepartment(inputDepartment);
+		return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+	}
+
+	@PutMapping(path = "/{departmentId}")
+	public ResponseEntity<DepartmentDTO> updateDepartmentById(@RequestBody @Valid DepartmentDTO departmentDTO, @PathVariable long departmentId) {
+		return ResponseEntity.ok(departmentService.updateDepartmentById(departmentId, departmentDTO));
+	}
+
+	@DeleteMapping(path = "/{departmentId}")
+	public ResponseEntity<Boolean> deleteDepartmentById(@PathVariable @Valid long departmentId) {
+		boolean gotDeleted = departmentService.deleteDepartmentById(departmentId);
+		if (gotDeleted) return ResponseEntity.ok(true);
+		return ResponseEntity.notFound().build();
+	}
+
+	@PatchMapping(path = "/{departmentId}")
+	public ResponseEntity<DepartmentDTO> updatePartialDepartmentById(@RequestBody @Valid Map<String, Object> updates, @PathVariable long departmentId) {
+		DepartmentDTO departmentDTO = departmentService.updatePartialDepartmentById(departmentId, updates);
+		if (departmentDTO == null) return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(departmentDTO);
+	}
+
 }
--- src/main/java/com/departmentProject/services/DepartmentService.java
+++ src/main/java/com/departmentProject/services/DepartmentService.java
@@ -2,12 +2,16 @@ package com.departmentProject.services;
 
 import com.departmentProject.dto.DepartmentDTO;
 import com.departmentProject.entities.DepartmentEntity;
+import com.departmentProject.exceptions.ResourceNotFoundException;
 import com.departmentProject.repositories.DepartmentRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -21,7 +25,7 @@ public class DepartmentService {
 		this.modelMapper = modelMapper;
 	}
 
-	public Optional<DepartmentDTO> getDepartmentByID(long id){
+	public Optional<DepartmentDTO> getDepartmentByID(long id) {
 		return departmentRepository.findById(id).map(departmentEntity -> modelMapper.map(departmentEntity, DepartmentDTO.class));
 	}
 
@@ -32,9 +36,39 @@ public class DepartmentService {
 				.map(departmentEntity1 -> modelMapper.map(departmentEntity1, DepartmentDTO.class))
 				.collect(Collectors.toList());
 	}
-	public DepartmentDTO createNewDepartment(@RequestBody DepartmentDTO inputDepartment){
+
+	public DepartmentDTO createNewDepartment(@RequestBody DepartmentDTO inputDepartment) {
 		DepartmentEntity toSaveEntity = modelMapper.map(inputDepartment, DepartmentEntity.class);
 		DepartmentEntity savedDepartment = departmentRepository.save(toSaveEntity);
 		return modelMapper.map(savedDepartment, DepartmentDTO.class);
+	}
+
+	public DepartmentDTO updateDepartmentById(long departmentId, DepartmentDTO departmentDTO) {
+		isExistByDepartmentById(departmentId);
+		DepartmentEntity departmentEntity = modelMapper.map(departmentId, DepartmentEntity.class);
+		departmentEntity.setId(departmentId);
+		DepartmentEntity saveDepartmentEntity = departmentRepository.save(departmentEntity);
+		return modelMapper.map(saveDepartmentEntity, DepartmentDTO.class);
+	}
+
+	public boolean deleteDepartmentById(long departmentId) {
+		isExistByDepartmentById(departmentId);
+		departmentRepository.deleteById(departmentId);
+		return true;
+	}
+
+	private void isExistByDepartmentById(long departmentId) {
+		boolean exist = departmentRepository.existsById(departmentId);
+		if (!exist) throw new ResourceNotFoundException("Department not found with id: " + departmentId);
+	}
+
+	public DepartmentDTO updatePartialDepartmentById(long departmentId, Map<String, Object> updates) {
+		isExistByDepartmentById(departmentId);
+		DepartmentEntity departmentEntity = departmentRepository.findById(departmentId).get();
+		updates.forEach((field, value) -> {
+			Field fieldToBeUpdated = ReflectionUtils.findRequiredField(DepartmentEntity.class, field);
+			ReflectionUtils.setField(fieldToBeUpdated, departmentEntity, value);
+		});
+		return modelMapper.map(departmentRepository.save(departmentEntity), DepartmentDTO.class);
 	}
 }
